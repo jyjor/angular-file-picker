2e7e39106337022de031161f44d773093696b899
GitHub Action <action@github.com>
2022-09-03 16:04:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/jyjor/angularfilepicker/MyBundle.kt
+++ src/main/kotlin/com/github/jyjor/angularfilepicker/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jyjor.angularfilepicker
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jyjor/angularfilepicker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jyjor/angularfilepicker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jyjor.angularfilepicker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jyjor.angularfilepicker.MyBundle
 
 class MyApplicationService {
 
